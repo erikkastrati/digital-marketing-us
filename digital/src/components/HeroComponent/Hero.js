e9004f15e800bc2139f0e0fb181df67bfefbaf96
Viktor Zdravkovski <viktorzdravkovski995@gmail.com>
2024-04-06 21:50:35 +0200

    Added Active Campaign form.

--- digital/src/components/HeroComponent/Hero.js
+++ digital/src/components/HeroComponent/Hero.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import "../HeroComponent/Hero.css";
 import GoogleReview from "../../assets/images/google-review.svg";
 import HeroTick from "../../assets/images/Group 44671.svg";
 
 const Hero = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [academy, setAcademy] = useState('Foundation Track');
+
+  const onChangeFullNameHandler = (event) => {
+    setFullName(event.target.value);
+  };
+
+  const onChangeEmailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangePhoneHandler = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const onSubmitFormHandler = (event) => {
+    setIsLoading(true);
+    event.preventDefault();
+
+    fetch(`https://akoskarova.activehosted.com/proc.php?u=570&f=570&s=&c=0&m=0&act=sub&v=2&or=8beb7dde5c1bb2035c2d2f64692a2764&fullname=${fullName}&email=${email}&phone=${phone}&field[35]=${academy}&jsonp=true`)
+        .then(() => {
+          setIsLoading(false);
+        });
+  };
+
   return (
     <div className="hero">
       <div className="hero__left">
@@ -39,28 +67,27 @@ const Hero = () => {
         />
       </div>
       <div className="hero__right">
-        <form className="hero__form">
+        <form className="hero__form" onSubmit={onSubmitFormHandler}>
           <h2>New Cohort Begins May 15th!</h2>
           <span>
             Complete the form and our Scholarship Specialist will be in touch!{" "}
           </span>
           <div className="form-group firstchild">
-            <input type="text" name="fullname" placeholder="Name and Surname" />
+            <input type="text" name="fullname" placeholder="Name and Surname" value={fullName} onChange={onChangeFullNameHandler}/>
           </div>
           <div className="form-group">
-            <input type="email" name="email" placeholder="Email Address" />
+            <input type="email" name="email" placeholder="Email Address" value={email} onChange={onChangeEmailHandler}/>
           </div>
           <div className="form-group">
-            <input type="text" name="phone" placeholder="Phone" />
+            <input type="text" name="phone" placeholder="Phone" value={phone} onChange={onChangePhoneHandler}/>
           </div>
           <div className="form-group">
-            <select name="academy">
-              <option value="">Choose A Program</option>
-              <option value="digitalMarketing"></option>
-              <option value="webDevelopment"></option>
-              <option value="graphicDesign"></option>
+            <select name="field[35]" value={academy} onChange={(event) => setAcademy(event.target.value)}>
+              <option value="Foundation Track" onClick={() => setAcademy("Foundation Track")}>Foundation Track</option>
+              <option value="Growth Track" onClick={() => setAcademy("Growth Track")}>Growth Track</option>
+              <option value="Leadership Track" onClick={() => setAcademy("Leadership Track")}>Leadership Track</option>
             </select>
-            <button type="submit">Apply Now</button>
+            <button type="submit" style={{backgroundColor: isLoading ? "gray" : "blue"}}>{isLoading ? "Applying..." : "Apply Now"}</button>
           </div>
         </form>
       </div>
